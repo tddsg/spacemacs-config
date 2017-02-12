5f7b1cc3e77e55eeeaf1055d63b6a51dda7f21a6
trungtq <taquangtrungvn@gmail.com>
2017-02-12 12:27:36 +0800

    minor

--- tddsg-init.el
+++ tddsg-init.el
@@ -557,6 +557,7 @@ If the new path's directories does not exist, create them."
 
   ;; backup
   (setq make-backup-files t)
+
   (setq make-backup-file-name-function 'tddsg-create-backup-file-name)
 
   ;; evil mode
@@ -583,6 +584,7 @@ If the new path's directories does not exist, create them."
   (eval-after-load "visual-line" (diminish 'visual-line-mode " ⤾"))
   (eval-after-load "merlin" '(diminish 'merlin-mode " ⓜ"))
   (eval-after-load "utop" '(diminish 'utop-minor-mode " ⓤ"))
+  (eval-after-load "magit-gitflow-mode" '(diminish 'magit-gitflow-mode " ⓕ"))
   (eval-after-load "flycheck" '(diminish 'flycheck-mode " ✔"))
   (eval-after-load "flyspell" '(diminish 'flyspell-mode " ✔"))
   (eval-after-load "holy-mode" '(diminish 'holy-mode " ⓗ"))
