bbba0e70415260c0a375358efcb3f9f323c1c894
trungtq <taquangtrungvn@gmail.com>
2018-08-10 14:22:27 +0800

    songbird

--- tddsg/local/songbird/songbird.el
+++ tddsg/local/songbird/songbird.el
@@ -32,6 +32,7 @@
         "lemma" "lemma_prop" "lemma_split" "lemma_test" "lemma_unsafe"
         "lemma_infer" "lemma_safe"
         "checkentail" "checkentail_exact" "checkentail_inexact"
+        "normalize"
         "check_nondet" "checksat" "checkunsat" "checkeq"
         "inferlemma" "inferentail" "inferrels" "unify" "unifyheap"))
 
